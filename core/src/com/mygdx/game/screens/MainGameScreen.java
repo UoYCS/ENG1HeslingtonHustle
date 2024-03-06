db19f41fe60b438fdaedd1f0aa78a77b4a46aa29
rs2218 <rs2218@york.ac.uk>
2024-03-06 13:16:54 +0000

    Added an orthographic camera that follows the player character

--- core/src/com/mygdx/game/screens/MainGameScreen.java
+++ core/src/com/mygdx/game/screens/MainGameScreen.java
@@ -3,28 +3,47 @@ package com.mygdx.game.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdx.game.HesHustle;
 
 public class MainGameScreen implements Screen {
 
-    public static float SPEED = 100;
+    public static float SPEED = 150;
 
     Texture img;
-    float x;
-    float y;
+    Sprite map;
+    float player_x;
+    float player_y;
+
+    final float GAME_WORLD_WIDTH = 1024;
+    final float GAME_WORLD_HEIGHT = 576;
+
+    OrthographicCamera camera;
 
     HesHustle game;
 
     public MainGameScreen (HesHustle game){
         this.game = game;
+
+        this.camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        this.camera.position.set(GAME_WORLD_WIDTH / 2, GAME_WORLD_HEIGHT / 2, 0);
+        this.camera.update();
+
+        this.game.camera = this.camera;
+        this.game.camera.position.set(player_x, player_y, 0);
+
     }
 
 
     @Override
     public void show() {
-        img = new Texture("badlogic.jpg");
+        map = new Sprite(new Texture("temp_map.png"));
+        map.setPosition(0,0);
+        map.setSize(GAME_WORLD_WIDTH, GAME_WORLD_HEIGHT);
+        img = new Texture("player.png");
     }
 
     @Override
@@ -47,18 +66,26 @@ public class MainGameScreen implements Screen {
             float horizontal_normalised = horizontal / length;
             float vertical_normalised = vertical / length;
 
-            y += vertical_normalised * SPEED * Gdx.graphics.getDeltaTime();
-            x += horizontal_normalised * SPEED * Gdx.graphics.getDeltaTime();
+            player_y += vertical_normalised * SPEED * Gdx.graphics.getDeltaTime();
+            player_x += horizontal_normalised * SPEED * Gdx.graphics.getDeltaTime();
         } else {
-
-            y += (vertical   * SPEED) * Gdx.graphics.getDeltaTime();
-            x += (horizontal * SPEED) * Gdx.graphics.getDeltaTime();
+            player_y += (vertical   * SPEED) * Gdx.graphics.getDeltaTime();
+            player_x += (horizontal * SPEED) * Gdx.graphics.getDeltaTime();
         }
 
 
-        ScreenUtils.clear(1, 0, 0, 1);
+        ScreenUtils.clear(255, 255, 255, 1);
+        game.camera.update();
+
         game.batch.begin();
-        game.batch.draw(img, x, y);
+
+        map.draw(game.batch);
+        //game.batch.draw(map_tex, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        game.camera.position.set(player_x, player_y, 0);
+        game.camera.update();
+
+        game.batch.setProjectionMatrix(game.camera.combined);
+        game.batch.draw(img, player_x, player_y);
         game.batch.end();
     }
 
