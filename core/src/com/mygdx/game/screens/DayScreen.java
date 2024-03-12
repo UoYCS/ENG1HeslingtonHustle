d97fb0525b7cc60dbff1cbc5f572109c3e45083b
DTRedshaw <dr1007@york.ac.uk>
2024-03-12 14:17:21 +0000

    Updating the end of day screen
    Updated the end of day screen to display more info, and also added a nice legend of zelda reference

--- core/src/com/mygdx/game/screens/DayScreen.java
+++ core/src/com/mygdx/game/screens/DayScreen.java
@@ -42,7 +42,8 @@ public class DayScreen implements Screen {
         dayBatch = new SpriteBatch();
         font = new BitmapFont();
         font.setColor(Color.WHITE);
-        font.getData().setScale(5); // Adjust the scale as needed
+        font.getData().setScale(3);
+
     }
 
     @Override
@@ -68,23 +69,70 @@ public class DayScreen implements Screen {
         // Calculate the position to center the text
         float screenWidth = Gdx.graphics.getWidth();
         float screenHeight = Gdx.graphics.getHeight();
+
+        // Displays the text for what day it is and the time remaining
         GlyphLayout layout = new GlyphLayout();
-        layout.setText(font, "Day " + day + " Complete");
-
-
-
-        font.draw(dayBatch, layout, (screenWidth - layout.width) / 2, (float) (screenHeight * 0.8));
 
         GlyphLayout days_left = new GlyphLayout();
+        GlyphLayout hours_left = new GlyphLayout();
 
-        if (day != 7) {
-            days_left.setText(font, (7 - day) + " Days till Exams!");
+        layout.setText(font, "Dawn of");
+        font.draw(dayBatch, layout, (screenWidth - layout.width) / 2, (float) (screenHeight * 0.95));
+
+        if (day == 0){
+            days_left.setText(font,  "The First Day");
+        }
+        if (day == 1){
+            days_left.setText(font,  "The Second Day");
+        }
+        else if (day == 2){
+            days_left.setText(font, "The Third Day");
+        }
+        else if (day == 3){
+            days_left.setText(font, "The Fourth Day");
+        }
+        else if (day == 4){
+            days_left.setText(font, "The Fifth Day");
+        }
+        else if (day == 5){
+            days_left.setText(font, "The Sixth Day");
+        }
+        else if (day == 6){
+            days_left.setText(font, "The Final Day");
         }
         else{
-            days_left.setText(font,  "Exams Over!");
+            days_left.setText(font, "The Exam Day");
         }
-        font.draw(dayBatch, days_left, (screenWidth - days_left.width) / 2, (float) (screenHeight * 0.5));
 
+        hours_left.setText(font, "-"+(24*(7 - day)) +" Hours Remain-");
+
+
+        font.draw(dayBatch, days_left, (screenWidth - days_left.width) / 2, (float) (screenHeight * 0.85));
+        font.draw(dayBatch, hours_left, (screenWidth - hours_left.width) / 2, (float) (screenHeight * 0.7));
+
+        // Displays the summary for what the user did in the day
+        GlyphLayout summary_title = new GlyphLayout();
+        GlyphLayout study = new GlyphLayout();
+        GlyphLayout eaten = new GlyphLayout();
+        GlyphLayout rec = new GlyphLayout();
+
+        summary_title.setText(font, "Summary for Day "+ day +":");
+        study.setText(font, "Times Studied: "+studyCounter[day-1]);
+
+        int countEat = 0;
+        for (int time:eatCounter[day-1]){
+            if (time != 0){
+                countEat++;
+            }
+        }
+
+        eaten.setText(font, "Times Eaten: "+countEat);
+        rec.setText(font, "Recreational Activities:"+recCounter[day-1]);
+
+        font.draw(dayBatch, summary_title, (screenWidth - summary_title.width) / 2, (float) (screenHeight * 0.5));
+        font.draw(dayBatch, study, (screenWidth - 750) / 2, (float) (screenHeight * 0.35));
+        font.draw(dayBatch, eaten, (screenWidth - 750) / 2, (float) (screenHeight * 0.25));
+        font.draw(dayBatch, rec, (screenWidth - 750) / 2, (float) (screenHeight * 0.15));
 
         dayBatch.end();
 
