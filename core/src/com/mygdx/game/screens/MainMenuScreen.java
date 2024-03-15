b59dba7852e4cf710e8e25ba9c9e7078fc00d845
rs2218 <rs2218@york.ac.uk>
2024-03-15 15:54:45 +0000

    Improved Main menu with buttons and functionality

--- core/src/com/mygdx/game/screens/MainMenuScreen.java
+++ core/src/com/mygdx/game/screens/MainMenuScreen.java
@@ -5,16 +5,30 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdx.game.HesHustle;
+import com.badlogic.gdx.math.Rectangle;
 
 public class MainMenuScreen implements Screen {
 
     private BitmapFont font;
-    private SpriteBatch dayBatch;
+    private SpriteBatch menuBatch;
+
+    Texture playButtonTexture;
+    Texture quitButtonTexture;
+
+    Texture muteButtonTexture;
+    Texture fullButtonTexture;
+
+    private Rectangle playButtonBounds;
+    private Rectangle quitButtonBounds;
+    private Rectangle muteButtonBounds;
+    private Rectangle fullButtonBounds;
 
     HesHustle game;
 
@@ -22,14 +36,44 @@ public class MainMenuScreen implements Screen {
 
         this.game = game;
 
-        dayBatch = new SpriteBatch();
+        menuBatch = new SpriteBatch();
         font = new BitmapFont();
-
-
-
         font.setColor(Color.WHITE);
         font.getData().setScale(5); // Adjust the scale as needed
+
+
+        playButtonTexture = new Texture(Gdx.files.internal("rectangle_button.png"));
+        quitButtonTexture = new Texture(Gdx.files.internal("rectangle_button.png"));
+
+        muteButtonTexture = new Texture(Gdx.files.internal("square_button.png"));
+        fullButtonTexture = new Texture(Gdx.files.internal("square_button.png"));
+
+
+        updateButtonBounds();
     }
+
+    private void updateButtonBounds() {
+        // Define the bounding rectangles for the buttons
+        float longButtonWidth = playButtonTexture.getWidth() * 1.5f;
+        float longButtonHeight = playButtonTexture.getHeight() * 1.5f;
+
+        float shortButtonWidth = muteButtonTexture.getWidth() * 1.5f;
+        float shortButtonHeight = muteButtonTexture.getHeight() * 1.5f;
+
+        float screenWidth = Gdx.graphics.getWidth();
+        float screenHeight = Gdx.graphics.getHeight();
+        float startX = (screenWidth - longButtonWidth * 2) / 2;
+
+        int offset = 30;
+
+        playButtonBounds = new Rectangle(startX - offset , (float) (screenHeight * 0.3), longButtonWidth, longButtonHeight);
+        quitButtonBounds = new Rectangle(startX + offset + longButtonWidth, (float) (screenHeight * 0.3), longButtonWidth, longButtonHeight);
+
+        int gap = 5;
+        muteButtonBounds = new Rectangle(screenWidth - shortButtonWidth * 2 - gap * 2, gap, shortButtonWidth, shortButtonHeight);
+        fullButtonBounds = new Rectangle(screenWidth - shortButtonWidth, gap, shortButtonWidth, shortButtonHeight);
+    }
+
 
     @Override
     public void show() {
@@ -39,27 +83,66 @@ public class MainMenuScreen implements Screen {
     @Override
     public void render(float delta) {
         ScreenUtils.clear(0, 0, 0, 1);
-        if (Gdx.input.isKeyJustPressed(Input.Keys.F)) {
-            ((Game) Gdx.app.getApplicationListener()).setScreen(new MainGameScreen(this.game));
+
+
+
+        menuBatch.begin();
+
+//        GlyphLayout layout = new GlyphLayout();
+//        layout.setText(font, "Main Menu\nPress F to Continue");
+//        font.draw(menuBatch, layout, (screenWidth - layout.width) / 2, (float) (screenHeight * 0.8));
+
+
+
+
+        menuBatch.draw(playButtonTexture, playButtonBounds.x, playButtonBounds.y, playButtonBounds.width, playButtonBounds.height);
+        menuBatch.draw(quitButtonTexture, quitButtonBounds.x, quitButtonBounds.y, quitButtonBounds.width, quitButtonBounds.height);
+
+        menuBatch.draw(muteButtonTexture, muteButtonBounds.x, muteButtonBounds.y, muteButtonBounds.width, muteButtonBounds.height);
+        menuBatch.draw(fullButtonTexture, fullButtonBounds.x, fullButtonBounds.y, fullButtonBounds.width, fullButtonBounds.height);
+
+        menuBatch.end();
+
+        if (Gdx.input.justTouched()) {
+            float touchX = Gdx.input.getX();
+            float touchY = Gdx.graphics.getHeight() - Gdx.input.getY(); // Invert Y-coordinate
+            if (playButtonBounds.contains(touchX, touchY)) {
+                ((Game) Gdx.app.getApplicationListener()).setScreen(new MainGameScreen(this.game));
+            } else if (quitButtonBounds.contains(touchX, touchY)) {
+                Gdx.app.exit();
+            } else if (muteButtonBounds.contains(touchX, touchY)) {
+                System.out.println("Mute");
+            } else if (fullButtonBounds.contains(touchX, touchY)) {
+                if (!Gdx.graphics.isFullscreen()) {
+                    Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
+                    ((Game) Gdx.app.getApplicationListener()).setScreen(new MainMenuScreen(this.game));
+                } else {
+                    Gdx.graphics.setWindowedMode(1024, 576); // Set your preferred window size
+                    ((Game) Gdx.app.getApplicationListener()).setScreen(new MainMenuScreen(this.game));
+                }
+                updateButtonBounds();
+
             }
+        }
 
-        dayBatch.begin();
 
-        // Calculate the position to center the text
-        float screenWidth = Gdx.graphics.getWidth();
-        float screenHeight = Gdx.graphics.getHeight();
-        GlyphLayout layout = new GlyphLayout();
-        layout.setText(font, "Main Menu\nPress F to Continue");
 
-        font.draw(dayBatch, layout, (screenWidth - layout.width) / 2, (float) (screenHeight * 0.8));
-
-        dayBatch.end();
 
     }
 
     @Override
     public void resize(int width, int height) {
+        updateButtonBounds();
 
+        menuBatch.begin();
+
+        menuBatch.draw(playButtonTexture, playButtonBounds.x, playButtonBounds.y, playButtonBounds.width, playButtonBounds.height);
+        menuBatch.draw(quitButtonTexture, quitButtonBounds.x, quitButtonBounds.y, quitButtonBounds.width, quitButtonBounds.height);
+
+        menuBatch.draw(muteButtonTexture, muteButtonBounds.x, muteButtonBounds.y, muteButtonBounds.width, muteButtonBounds.height);
+        menuBatch.draw(fullButtonTexture, fullButtonBounds.x, fullButtonBounds.y, fullButtonBounds.width, fullButtonBounds.height);
+
+        menuBatch.end();
     }
 
     @Override
